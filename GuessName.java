4145ad3d958b081f7f2269ccb9e6a4acd3f098af
zena <e.nosazena@gmail.com>
2014-09-20 20:00:04 -0400

    guess name

---
+++ GuessName.java
@@ -0,0 +1,42 @@
+package GuessingGame;
+
+import java.util.*;
+
+public class GuessName {
+
+	public static void main(String[] args)
+	{
+		Scanner input=new Scanner(System.in);
+		System.out.println("1-100 arasında bir sayi tutunuz");
+
+		int tahmin=0;
+		int lower=1;
+		int upper=100;
+		int sayac=0;
+		int secim=3;
+		tahmin=(lower+upper)/2;
+		while(secim!= 0)
+		{	
+			tahmin=(lower+upper)/2;
+			System.out.printf("Guessing: %d (0:right guess 1:my number is lower 2:my number is higher) : ", tahmin);
+		    secim=input.nextInt();
+			if(secim==0)
+			{
+				System.out.println("BULUNDUU");
+				sayac++;
+				break;
+			}
+			else if(secim==1)
+			{
+				upper=tahmin;
+				sayac++;
+			}
+			else if(secim==2)
+			{
+				lower=tahmin;
+				sayac++;
+			}
+		}
+	}
+
+}
